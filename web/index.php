5d5523eaf7a26499bcb1063526e6dffb1f567da3
Volodymyr Kravchuk <pulsarv@rambler.ru>
2015-11-03 08:56:46 +0200

    temp

--- web/index.php
+++ web/index.php
@@ -15,16 +15,34 @@ use Entity\OrderItem;
 $dbHandler = Connector::connect($config['host'], $config['port'], $config['db_name'], $config['db_user'], $config['db_password']);
 
 $customers = new CustomersManager($dbHandler);
+$customersResult = $customers->findAll();
 $books = new BooksManager($dbHandler);
+$booksResult = $books->findAll();
 $orders = new OrdersManager($dbHandler);
-//$orderItems = new OrderItemsManager($dbHandler);
+$ordersResult = $orders->findAll();
+$orderItems = new OrderItemsManager($dbHandler);
+$orderItemsResult = $orderItems->findAll();
+
+$booksOutPut = '<tr><th>ISBN</th><th>Author</th><th>Title</th><th>Price</th></tr>';
+
+foreach ($booksResult as $i) {
+    $booksOutPut .= '<tr><td>' . $i->getIsbn() . '</td><td>' . $i->getAuthor() . '</td><td>' . $i->getTitle() . '</td><td>' . $i->getPrice() . '</td></tr>';
+}
 
 
-//$order1 = new Order('2', '55.44', '5');
-//$customer1->setCustomerId($customers->insert($customer1));
-//$customer1->setAddress('вул.Чехова');
-//$customers->update($customer1);
-$r = $orders->findAll();
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('template');
+$twig = new Twig_Environment($loader);
+$indexPage = $twig->loadTemplate('index.html.twig');
+echo $indexPage->render(array(
+    'title' => 'Структура бази даних',
+    'tableBook' => 'Book',
+    'tableBookData' => Array($booksOutPut),
+));
+
+
+
+/*$r = $orders->findAll();
 foreach ($r as $o) {
     echo $o->getOrderId();
     echo $o->getCustomerId();
